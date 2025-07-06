a3c089e03b4843e514fe0b3b3cf9c9c8d761fca9
Ben Brackenbury <benbrack22@icloud.com>
2025-07-06 18:20:35 +0100

    feat: register DB Context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using ASP_Chat.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// register DB Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
